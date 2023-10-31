41425812f34cb51189b02cded28d20ab2eb01243
Madhav-07 <m.madhav12345679@gmail.com>
2023-10-31 13:18:12 +0530

    Pain

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
         int choice = 0;
+        Scanner sc = new Scanner(System.in);
         while(choice != 5) {
             System.out.println("\nWelcome to the Calculator Program:" );
             System.out.println("1. Addition");
@@ -14,7 +15,6 @@ public class Main {
             System.out.println("4. Division");
             System.out.println("5. Exit");
             System.out.print("Enter Choice: ");
-            Scanner sc = new Scanner(System.in);
             choice = sc.nextInt();
             switch(choice) {
                 case 1:
@@ -34,7 +34,7 @@ public class Main {
                 default:
                     System.out.println("Please enter valid choice");
             }
-            sc.close();
         }
+        sc.close();
     }
 }
